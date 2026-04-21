726ef3621d780e199490eff09157393d0bc4930d
Kass-ii <kassidy.satterfield@outlook.com>
2026-04-20 22:15:40 -0500

    test CHANGED PORT TO 5001

--- src/backend/app.py
+++ src/backend/app.py
@@ -451,4 +451,5 @@ def dart_alerts():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    #app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=False)
