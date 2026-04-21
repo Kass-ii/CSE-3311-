6496e67360346016209bf154aa9196e50499d6b7
Kass-ii <kassidy.satterfield@outlook.com>
2026-04-20 22:13:27 -0500

    test

--- src/backend/gtfs_parser.py
+++ src/backend/gtfs_parser.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "data")
+#DATA_PATH = os.path.join(BASE_DIR, "data")
+DATA_PATH = "../data/"
 
 
 
